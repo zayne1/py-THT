ff1f0e40518d88e2f4e9208b79a26974c2f3433b
Zayne Arnold <Zayne.a@gmail.com>
2022-09-13 18:16:55 +0200

    add comments to script

--- zayne-THT-MagicSquares.py
+++ zayne-THT-MagicSquares.py
@@ -39,7 +39,7 @@ def check_bottom_field_empty(cur_row, cur_col, list, box_size):
     if not bool(list[min(box_size-1, cur_row+1)][cur_col]):
         return True
 
-
+# display
 def print_magic_square(list):
     print("\r\n\r\n\r\n")
     print('    ███╗░░░███╗░█████╗░░██████╗░██╗░█████╗░  ░██████╗░██████╗░██╗░░░██╗░█████╗░██████╗░███████╗░██████╗')
@@ -115,6 +115,7 @@ def validate(magic_constant, list, box_size):
 
 
 def main():
+    # We use a try block to capture the string input error, so that we can show a nicer err mesg
     try:
         box_size = int(input("Enter a positive, odd number: "))
 
@@ -123,7 +124,7 @@ def main():
         cur_col=int((box_size-1)/2) # start at middle index item of our odd number
         cur_row=0
 
-        if box_size % 2 !=0 and box_size > 0:    
+        if box_size % 2 !=0 and box_size > 0: # validate number
             magic_constant = box_size * ((box_size*box_size) + 1)/2 # 15 for 3, 65 for 15
             list = create_list(box_size)
 
@@ -132,38 +133,66 @@ def main():
 
             for i in range(2, box_slots+1):
 
+                # if top right field is empty..
                 if check_top_right_field_empty(cur_row, cur_col,list,box_size):
-                    list[cur_row-1][cur_col+1]=i
+                    
+                    list[cur_row-1][cur_col+1]=i # enter num into top left field
+                    
+                    # move our current col/row pointer to where it we just worked
                     cur_row=max(0, cur_row-1)
                     cur_col=min(box_size-1, cur_col+1)
 
+                # if we're at top row
                 elif check_is_top_row(cur_row) and check_at_right_border(cur_col, box_size):
+                    
+                    # test if bottom field is emptu
                     if check_bottom_field_empty(cur_row, cur_col, list,box_size):
-                        list[min(box_size-1, cur_row+1)][cur_col]=i
+
+                        list[min(box_size-1, cur_row+1)][cur_col]=i # enter num into bottom field
+                        
+                        # move our current col/row pointer to where it we just worked
                         cur_row=min(box_size-1, cur_row+1)
 
+                # if we're at top row
                 elif check_is_top_row(cur_row):
+
+                    # check if bottom row, next col has no val
                     if not bool(list[last_row][min(box_size-1, cur_col+1)]):
-                        list[last_row][cur_col+1]=i
+                        
+                        list[last_row][cur_col+1]=i # enter num into last row, next col
+
+                        # move our current col/row pointer to where it we just worked
                         cur_row=last_row
                         cur_col=min(box_size-1, cur_col+1)
                     else:
                         continue
 
+                # if we're at right most col
                 elif check_at_right_border(cur_col, box_size):
-                    if not bool(list[cur_row-1][0]):
-                        list[cur_row-1][0]=i
+                    
+                    # check if prev row, first col has no val
+                    if not bool(list[cur_row-1][0]): 
+                        
+                        list[cur_row-1][0]=i # enter num into prev row, left most col
+
+                        # move our current col/row pointer to where it we just worked
                         cur_row=max(0, cur_row-1)
                         cur_col=0
                     else:
                         continue
 
+                #is bottom field empty?
                 elif check_bottom_field_empty(cur_row, cur_col, list,box_size):
-                    list[min(box_size-1, cur_row+1)][cur_col]=i
+                    
+                    list[min(box_size-1, cur_row+1)][cur_col]=i # enter num into bottom row of current col
+
+                    # move our current col/row pointer to where it we just worked
                     cur_row=min(box_size-1, cur_row+1)
 
+            # display graphic output
             print_magic_square(list)
             
+            # validate if a true magic num
             if validate(magic_constant, list, box_size):
                 print("correct \r\n\r\n\r\n")
             else:
